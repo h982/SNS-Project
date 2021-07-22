29c2ec7519a3602e3996b287e7dc267e4dc363d3
bumhee <qqq960909@likelion.org>
2021-07-22 15:36:09 +0900

    S05P12C105-99 update teampage

--- frontbase/src/api/index.js
+++ frontbase/src/api/index.js
@@ -3,10 +3,9 @@ import axios from "axios";
 //import http from '@/util/http-common';
 function createInstance() {
   const instance = axios.create({
-    baseURL: "http://localhost:8080",
     headers: {
       "Content-Type": "application/json"
-    }
+    },
   });
   return instance;
 }
