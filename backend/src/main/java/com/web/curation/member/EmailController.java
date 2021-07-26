06b79406ee31f91e2402b298679b5c22cbe85815
양동현 <y7248527@naver.com>
2021-07-26 10:09:49 +0900

    S05P12C105-118 feat: 이메일 인증 수정

--- backend/src/main/java/com/web/curation/member/EmailController.java
+++ backend/src/main/java/com/web/curation/member/EmailController.java
@@ -52,7 +52,7 @@ public class EmailController {
 				" <div" 																																																	+ 
 				"	style=\"font-family: 'Apple SD Gothic Neo', 'sans-serif' !important; width: 400px; height: 600px; border-top: 4px solid #02b875; margin: 100px auto; padding: 30px 0; box-sizing: border-box;\">"		+ 
 				"	<h1 style=\"margin: 0; padding: 0 5px; font-size: 28px; font-weight: 400;\">"																															+ 
-				"		<span style=\"font-size: 15px; margin: 0 0 10px 3px;\">YG1110 BLOG</span><br />"																													+ 
+				"		<span style=\"font-size: 15px; margin: 0 0 10px 3px;\">혼자 머하니?</span><br />"																													+ 
 				"		<span style=\"color: #02b875\">메일인증</span> 안내입니다."																																				+ 
 				"	</h1>\n"																																																+ 
 				"	<p style=\"font-size: 16px; line-height: 26px; margin-top: 50px; padding: 0 5px;\">"																													+ 
