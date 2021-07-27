0f307b4a9cf0ced175b62ff28662bf9fe247d552
hohwang <ghghkd52@naver.com>
2021-07-27 18:13:38 +0900

    feat : PhotoDto 구현 (추가작업-add column imageName )

---
+++ backend/src/main/java/com/web/curation/files/PhotoDto.java
@@ -0,0 +1,39 @@
+package com.web.curation.files;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "Photo")
+public class PhotoDto {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long photoId;
+
+    @Column(name = "original_filename")
+    private String originalName;
+
+    @Column(name = "file_path")
+    private String filePath;
+
+    @Column(name = "file_size")
+    private Long fileSize;
+
+    private String imageName;
+
+    private String imageExtension;
+
+
+    @Builder
+    public PhotoDto(String originalName, String filePath, Long fileSize,String imageName,String imageExtension) {
+        this.originalName = originalName;
+        this.filePath = filePath;
+        this.fileSize = fileSize;
+        this.imageName = imageName;
+        this.imageExtension = imageExtension;
+    }
+}
