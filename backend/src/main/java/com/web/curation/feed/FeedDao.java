386a682cd5947722b42cc3b7a949f74074928486
hohwang <ghghkd52@naver.com>
2021-07-29 15:07:26 +0900

    feat : FeedDao Fetch join 추가

--- backend/src/main/java/com/web/curation/feed/FeedDao.java
+++ backend/src/main/java/com/web/curation/feed/FeedDao.java
@@ -7,6 +7,9 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface FeedDao extends JpaRepository<Feed, Long>{
 	Feed getFeedByFeedId(int feedId);
+
+	@Query("SELECT f FROM Feed f join fetch f.photos")
+	List<Feed> findAllJoinFetch();
 	
 //	List<Feed> findFeedByMemberId(int memberId);
 //	List<Feed> findFeedByMemberIdAndTeamId(int memberId, int teamId);
