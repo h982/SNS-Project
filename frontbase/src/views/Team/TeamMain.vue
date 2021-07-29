cc7b2708e170da5691465d9755e4738e90b574cb
bumhee <qqq960909@likelion.org>
2021-07-29 18:42:32 +0900

    feat: join group

--- frontbase/src/views/Team/TeamMain.vue
+++ frontbase/src/views/Team/TeamMain.vue
@@ -34,20 +34,20 @@
       </v-bottom-navigation>
     </v-layout>
     <v-layout column justify-center class="mt-4 pt-2">
-      <h1 class="text-xs-center mb-4 pb-2">{{selectTeam.name}}</h1>
+      <h1 class="text-xs-center mb-4 pb-2">{{selectTeam.name.replaceAll("\"", "")}}</h1>
       <br>
       <v-img :src="selectTeam.photoDto.filePath" aspect-ratio="2.75" height="330" contain></v-img>
       <v-layout column justify-center align-center class="mt-4 pt-2">
         <h2>팀 소개</h2>
         <v-flex wrap justify-center align-center class="textbox">
           <h3>
-            {{selectTeam.introduction}}
+            {{selectTeam.introduction.replaceAll("\"", "")}}
           </h3>
         </v-flex>
         <br>
 
         <h2>팀장</h2>
-        <p>{{selectTeam.leader}}</p>
+        <p>{{selectTeam.leader.replaceAll("\"", "")}}</p>
         <br>
         <br>
         <v-btn @click="join" type="button" color="green" class="white--text">가입하기</v-btn>
@@ -75,11 +75,18 @@
 <script>
 import { mapGetters } from 'vuex';
 import TeamHeader from "@/components/TeamHeader"
+import { createInstance } from "@/api/index.js";
+
 export default {
   computed:{
-    ...mapGetters(["selectTeam","memberInfo"]),
+    ...mapGetters(["selectTeam","memberInfo","myTeamList"]),
   },
-  
+  created() {
+    console.log(this.selectTeam);
+    // console.log(this.memberInfo);
+    this.$store.dispatch("GET_MY_TEAM_INFO",this.memberInfo.memberId);
+    console.log(this.myTeamList);
+  },
   data() {
     return {
     };
@@ -87,10 +94,30 @@ export default {
   components: {
     TeamHeader
   },
-  
   methods: {
     join() {
-      alert("가입신청하시겠습니까?");
+      const instance = createInstance();
+      instance
+      .post("/request", {
+        "member":{
+          "memberId": this.memberInfo.memberId,
+        },
+        "team":{
+          "teamId": this.selectTeam.teamId
+        }
+      })
+      .then(
+        (response) => {
+          if (response.data.message === "success") {
+            alert("가입 요청 완료");
+          } else {
+            alert("가입 요청 실패");
+          }
+        }
+      )
+      .catch(() => {
+        alert("에러");
+      });
     },
     moveMain(){
       this.$router.push("/teammain");
@@ -106,7 +133,7 @@ export default {
     },
     moveTeamFeed(){
       this.$router.push("/teamFeed");
-    }
+    },
   },
 }
 </script>
