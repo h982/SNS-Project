51bcb587375f98835660b9cc012af2268d69de33
hohwang <ghghkd52@naver.com>
2021-07-29 15:08:15 +0900

    feat : FeedService getFeedList 추가

--- backend/src/main/java/com/web/curation/feed/FeedService.java
+++ backend/src/main/java/com/web/curation/feed/FeedService.java
@@ -1,17 +1,25 @@
 package com.web.curation.feed;
 
 import com.web.curation.files.PhotoDto;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@AllArgsConstructor
 public class FeedService {
 
-	@Autowired
 	FeedDao feedDao;
 	
 	public Feed registerFeed(Feed feed) {
 		return feedDao.save(feed);
 	}
+
+	public List<Feed> getFeedList(){
+		return feedDao.findAllJoinFetch();
+	}
+
 	
 }
