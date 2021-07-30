029ef0ca1365550ca4aca0a7c85ea0c32cb5f2fe
hohwang <ghghkd52@naver.com>
2021-07-30 10:46:10 +0900

    refactor : 불필요한 주석 제거

--- backend/src/main/java/com/web/curation/feed/FeedDao.java
+++ backend/src/main/java/com/web/curation/feed/FeedDao.java
@@ -1,16 +1,11 @@
 package com.web.curation.feed;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface FeedDao extends JpaRepository<Feed, Long>{
-	Feed getFeedByFeedId(int feedId);
+import java.util.List;
 
-	@Query("SELECT f FROM Feed f join fetch f.photos")
+public interface FeedDao extends JpaRepository<Feed, Long>{
+	@Query("SELECT f FROM Feed f join fetch f.photos ORDER BY f.writeDate DESC")
 	List<Feed> findAllJoinFetch();
-	
-//	List<Feed> findFeedByMemberId(int memberId);
-//	List<Feed> findFeedByMemberIdAndTeamId(int memberId, int teamId);
 }
