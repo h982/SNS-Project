dfef3db4a0be6db7fd92b0f602b06cd2c2a74830
ericharry <ericharry@naver.com>
2021-08-03 11:45:20 +0900

    [S05P13C105-74][BE] feat : controller, service 클린 작업

--- backend/src/main/java/com/web/curation/feed/FeedService.java
+++ backend/src/main/java/com/web/curation/feed/FeedService.java
@@ -1,17 +1,59 @@
 package com.web.curation.feed;
 
+import com.web.curation.amazonS3.S3Uploader;
+import com.web.curation.files.PhotoDao;
+import com.web.curation.files.PhotoDto;
+import com.web.curation.files.PhotoService;
+import com.web.curation.team.challenge.TeamChallenge;
+import com.web.curation.team.challenge.TeamChallengeDao;
+import com.web.curation.team.join.JoinTeam;
+import com.web.curation.team.join.JoinTeamDao;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class FeedService {
+	private FeedDao feedDao;
+	private PhotoDao photoDao;
+	private S3Uploader s3Uploader;
+	private JoinTeamDao joinTeamDao;
+	private TeamChallengeDao teamChallengeDao;
 
-	FeedDao feedDao;
-	
-	public Feed registerFeed(Feed feed) {
+	public Feed registerFeed(FeedDto feedDto) throws IOException {
+		//Feed 객체 생성하기
+		Feed feed = Feed.builder()
+				.joinTeam(new JoinTeam(feedDto.getJoinTeamId()))
+				.teamchallenge(null)
+				.teamName(feedDto.getTeamName())
+				.contents(feedDto.getContents())
+				.writer(feedDto.getWriter())
+				.build();
+		if(feedDto.getTeamchallengeId() != 0){
+			feed.setTeamchallenge(new TeamChallenge(feedDto.getTeamchallengeId()));
+		}
+		System.out.println(feed);
+		Feed resultFeed = feedDao.save(feed);
+
+		//사진 등록
+		List<PhotoDto> photoList = new ArrayList<>();
+//		for(MultipartFile multipartFile : feedDto.getImages()){
+//			PhotoDto uploadPhoto = s3Uploader.upload(multipartFile,"static");
+//			uploadPhoto.setFeed(resultFeed);
+//			photoList.add(photoDao.save(uploadPhoto));
+//		}
+		PhotoDto uploadPhoto = s3Uploader.upload(feedDto.getImage(), "static");
+
+		uploadPhoto.setFeed(resultFeed);
+		photoList.add(photoDao.save(uploadPhoto));
+		feed.setPhotos(photoList);
+
 		return feedDao.save(feed);
 	}
 
@@ -19,5 +61,23 @@ public class FeedService {
 		return feedDao.findAllJoinFetch();
 	}
 
-	
+	public boolean updateFeed(FeedDto feedDto){
+		Optional<Feed> chkFeed = feedDao.findById(new Long(feedDto.getFeedId()));
+		if(!chkFeed.isPresent()){
+			return false;
+		}
+		Feed feed = Feed.builder()
+				.feedId(feedDto.getFeedId())
+				.contents(feedDto.getContents())
+				.joinTeam(joinTeamDao.findById(feedDto.getJoinTeamId()).get())
+				.teamName(feedDto.getTeamName())
+				.writer(feedDto.getWriter())
+//				.photos()
+				.teamchallenge(teamChallengeDao.getTeamChallengeByTeamChallengeId(feedDto.getTeamchallengeId()))
+				.build();
+
+		feedDao.save(feed);
+		return true;
+	}
+
 }
