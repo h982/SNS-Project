e2f845273634ecf10e00679a56ee9ad10f125866
hohwang <ghghkd52@naver.com>
2021-08-03 12:16:15 +0900

    [S05P13C105-92][BE] refactor: Sport Entity, Dto 분리

--- backend/src/main/java/com/web/curation/sport/SportDao.java
+++ backend/src/main/java/com/web/curation/sport/SportDao.java
@@ -2,6 +2,6 @@ package com.web.curation.sport;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SportDao extends JpaRepository<SportDto,Integer> {
-    SportDto findById(int sportId);
+public interface SportDao extends JpaRepository<Sport,Integer> {
+    Sport findById(int sportId);
 }
