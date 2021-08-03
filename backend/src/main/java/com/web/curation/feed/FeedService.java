30158d969365370fa97f92a832a50f6ac8e93ab0
ericharry <ericharry@naver.com>
2021-08-03 13:39:24 +0900

    [S05P13C105-61][BE] feat : 피드 수정 기능 추가

--- backend/src/main/java/com/web/curation/feed/FeedService.java
+++ backend/src/main/java/com/web/curation/feed/FeedService.java
@@ -3,15 +3,12 @@ package com.web.curation.feed;
 import com.web.curation.amazonS3.S3Uploader;
 import com.web.curation.files.PhotoDao;
 import com.web.curation.files.PhotoDto;
-import com.web.curation.files.PhotoService;
 import com.web.curation.team.challenge.TeamChallenge;
 import com.web.curation.team.challenge.TeamChallengeDao;
 import com.web.curation.team.join.JoinTeam;
 import com.web.curation.team.join.JoinTeamDao;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -61,22 +58,54 @@ public class FeedService {
 		return feedDao.findAllJoinFetch();
 	}
 
-	public boolean updateFeed(FeedDto feedDto){
-		Optional<Feed> chkFeed = feedDao.findById(new Long(feedDto.getFeedId()));
+	public boolean updateFeed (FeedDto feedDto)throws IOException{
+		Optional<Feed> chkFeed = feedDao.findById(feedDto.getFeedId());
 		if(!chkFeed.isPresent()){
 			return false;
 		}
+		Feed oldFeed = chkFeed.get();
+		for(PhotoDto photo : oldFeed.getPhotos()) {
+			s3Uploader.deleteFile(photo.getImageName());
+			photoDao.delete(photo);
+		}
+
+		List<PhotoDto> photoList = new ArrayList<>();
+//		for(MultipartFile multipartFile : feedDto.getImages()){
+//			PhotoDto uploadPhoto = s3Uploader.upload(multipartFile,"static");
+//			uploadPhoto.setFeed(resultFeed);
+//			photoList.add(photoDao.save(uploadPhoto));
+//		}
+		PhotoDto uploadPhoto = s3Uploader.upload(feedDto.getImage(), "static");
+
+		uploadPhoto.setFeed(oldFeed);
+		photoList.add(photoDao.save(uploadPhoto));
+
 		Feed feed = Feed.builder()
 				.feedId(feedDto.getFeedId())
 				.contents(feedDto.getContents())
 				.joinTeam(joinTeamDao.findById(feedDto.getJoinTeamId()).get())
 				.teamName(feedDto.getTeamName())
 				.writer(feedDto.getWriter())
-//				.photos()
+				.photos(photoList)
 				.teamchallenge(teamChallengeDao.getTeamChallengeByTeamChallengeId(feedDto.getTeamchallengeId()))
 				.build();
 
 		feedDao.save(feed);
+		return true;
+	}
+
+	public boolean deleteFeed(FeedDto feedDto){
+		Optional<Feed> chkFeed = feedDao.findById(feedDto.getFeedId());
+		if(!chkFeed.isPresent()){
+			return false;
+		}
+		Feed feed = chkFeed.get();
+		for(PhotoDto photo : feed.getPhotos()){
+			s3Uploader.deleteFile(photo.getImageName());
+			photoDao.delete(photo);
+		}
+
+		feedDao.delete(feed);
 		return true;
 	}
 
