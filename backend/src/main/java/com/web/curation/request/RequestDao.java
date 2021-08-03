9bc3b913a28c82cd38c54e9a2b12643d65d6c4e4
mev01 <mevolicus@gmail.com>
2021-08-03 14:35:48 +0900

    [S05P13C105-101][BE] refactor : Request 객체 제외 전부 수정

--- backend/src/main/java/com/web/curation/request/RequestDao.java
+++ backend/src/main/java/com/web/curation/request/RequestDao.java
@@ -7,12 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.web.curation.member.Member;
+import com.web.curation.team.Team;
 import com.web.curation.team.TeamDto;
 
 public interface RequestDao extends JpaRepository<Request, Integer>{
-	public Optional<Request> getRequestByTeamAndMember(TeamDto team, Member member);
-	public List<Request> findAllByTeam(TeamDto team);
-//	@Query(value = "select * from Request where team_id = :teamId and member_id = :memberId", nativeQuery = true)
-//	public Optional<Request> getRequestByTeamIdAndMemberId(long teamId, int memberId);
-//	public List<Request> getRequestByTeamId(int teamId);
+	public Optional<Request> getRequestByTeamAndMember(Team team, Member member);
+	public List<Request> findAllByTeam(Team team);
 }
