8f3115e6c34e6ac4b15e0bf2e1f6b0a25858118a
hohwang <ghghkd52@naver.com>
2021-08-04 15:13:22 +0900

    [S05P13C105-33][BE] feat: Comment 작성, 삭제, 조회 기능 구현

--- backend/src/main/java/com/web/curation/feed/comment/CommentDao.java
+++ backend/src/main/java/com/web/curation/feed/comment/CommentDao.java
@@ -1,11 +1,17 @@
 package com.web.curation.feed.comment;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 
-public interface CommentDao {
+public interface CommentDao extends JpaRepository<Comment, Integer> {
 	// 피드의 댓글 찾기
-	List<Comment> findCommentByFeedId(int feedId);
+//	List<Comment> findCommentByFeedId(int feedId);
 
 	// 대댓글 찾기
-	List<Comment> findCommentByParentId(int parentId);
+//	List<Comment> findCommentByParentId(int parentId);
+//	@Query("SELECT c FROM Comment c join fetch c.coComments")
+	@Query("select c from Comment c where c.feed.feedId = :feedId")
+	List<Comment> findAllByFeed_Id(int feedId);
 }
