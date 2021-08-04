3b4a53efb0ae75c483cce2344b8eab517d00e1be
mev01 <mevolicus@gmail.com>
2021-08-04 17:35:09 +0900

    [S05P13C105-136][BE] fix : conflict 해결 오류 수정

--- backend/src/main/java/com/web/curation/feed/comment/CommentDao.java
+++ backend/src/main/java/com/web/curation/feed/comment/CommentDao.java
@@ -4,21 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-
-<<<<<<< backend/src/main/java/com/web/curation/feed/comment/CommentDao.java
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.web.curation.feed.Feed;
-import com.web.curation.feed.FeedDao;
 import com.web.curation.member.Member;
 
 public interface CommentDao extends JpaRepository<Comment, Integer>{
-	// 피드의 댓글 찾기
-//	List<Comment> findCommentByFeedId(int feedId);
-
-	// 대댓글 찾기
-//	List<Comment> findCommentByParentId(int parentId);
-//	@Query("SELECT c FROM Comment c join fetch c.coComments")
 	@Query("select c from Comment c where c.feed.feedId = :feedId")
 	List<Comment> findAllByFeed_Id(int feedId);
 
