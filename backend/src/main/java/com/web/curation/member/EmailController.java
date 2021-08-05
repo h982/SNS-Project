66c673ca7f8e7e1a9f0fc34e157121a3a3269cf6
KingBlackCow <sgs1159@naver.com>
2021-08-05 14:53:51 +0900

    [S05P13C105-162][BE] feat : 회원가입시 유저 이메일 인증서비스 개발

--- backend/src/main/java/com/web/curation/member/EmailController.java
+++ backend/src/main/java/com/web/curation/member/EmailController.java
@@ -11,13 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.web.curation.model.BasicResponse;
 import com.web.curation.model.NormalResponse;
@@ -34,15 +28,16 @@ public class EmailController {
 	private EmailService emailService;
 	@Autowired
 	private MemberService memberService;
-	
+
+	private static int authenticNum;
 	@PostMapping(value = "/send")
 	@ApiOperation(value = "이메일 전송")
 	@ApiResponses(value = {@ApiResponse(code = 200, message = "이메일 정상 전송", response = NormalResponse.class)})
-	public ResponseEntity<Map<String, Object>> sendEmail(@RequestBody MemberDto member) throws MessagingException {
+	public ResponseEntity<Map<String, Object>> sendEmail(@RequestParam(name = "member_email") String memberEmail) throws MessagingException {
 		Map<String, Object> resultMap = new HashMap<>();
     	HttpStatus status = null;
 		StringBuffer emailcontent = new StringBuffer();
-		
+		authenticNum = (int)(Math.random()*8999) +1000;
 		emailcontent.append("<!DOCTYPE html>");
 		emailcontent.append("<html>");
 		emailcontent.append("<head>");
@@ -56,24 +51,21 @@ public class EmailController {
 				"		<span style=\"color: #02b875\">메일인증</span> 안내입니다."																																				+ 
 				"	</h1>\n"																																																+ 
 				"	<p style=\"font-size: 16px; line-height: 26px; margin-top: 50px; padding: 0 5px;\">"																													+ 
-				member.getName()																																																+
-				"		님 안녕하세요.<br />"																																													+ 
+				"		안녕하세요.<br />"																																													+
 				"		혼자 머하니?에 가입해 주셔서 진심으로 감사드립니다.<br />"																																						+ 
-				"		아래 <b style=\"color: #02b875\">'메일 인증'</b> 버튼을 클릭하여 회원가입을 완료해 주세요.<br />"																													+ 
+				"		아래 <b style=\"color: #02b875\">'인증번호'</b> 를 입력하여 회원가입을 완료해 주세요.<br />"																													+
 				"		감사합니다."																																															+ 
-				"	</p>"																																																	+ 
-				"	<a style=\"color: #FFF; text-decoration: none; text-align: center;\""																																	+
-				"	href=\"http://localhost:8080/email/certified?email=" + member.getEmail() +  "\" target=\"_blank\">"														+ 
-				"		<p"																																																	+
-				"			style=\"display: inline-block; width: 210px; height: 45px; margin: 30px 5px 40px; background: #02b875; line-height: 45px; vertical-align: middle; font-size: 16px;\">"							+ 
-				"			메일 인증</p>"																																														+ 
-				"	</a>"																																																	+
-				"	<div style=\"border-top: 1px solid #DDD; padding: 5px;\"></div>"																																		+
+				"	</p>"																																																	+
+						"	<h1 style=\"margin: 0; padding: 0 5px; font-size: 28px; font-weight: 400;\">"																															+
+						"		<span style=\"font-size: 15px; margin: 0 0 10px 3px;\">인증번호</span><br />"																													+
+						"		<span style=\"color: #02b875\"> "+authenticNum +"</span>"																																		+
+						"	</h1>\n"	+
+    			"<div style=\"border-top: 1px solid #DDD; padding: 5px;\"></div>"																																		+
 				" </div>"
 		);
 		emailcontent.append("</body>");
 		emailcontent.append("</html>");
-		emailService.sendMail("admin", member.getEmail() + " <" + member.getEmail() + ">", "혼자 머하니 이메일 인증", emailcontent.toString());
+		emailService.sendMail("admin", memberEmail + " <" + memberEmail + ">", "혼자 머하니 이메일 인증", emailcontent.toString());
 		
 		resultMap.put("message", "success");
     	status = HttpStatus.OK;
@@ -82,20 +74,19 @@ public class EmailController {
 	}
 	
 	@GetMapping(value = "/certified")
-	@Transactional
 	@ApiOperation(value = "이메일 인증")
 	@ApiResponses(value = {@ApiResponse(code = 200, message = "이메일 인증 정상 작동", response = NormalResponse.class)})
-	public ResponseEntity<Map<String, Object>> checkEmail(String email) throws MessagingException {
+	public ResponseEntity<Map<String, Object>> checkEmail(@RequestParam(name = "num") int num) throws MessagingException {
 		Map<String, Object> resultMap = new HashMap<>();
     	HttpStatus status = null;
     	
     	System.out.println("check");
-		
-		memberService.certifyMemberByEmail(email);
-		Optional<MemberDto> member = memberService.getUser(email);
-		
-		resultMap.put("member", member);
-		resultMap.put("message", "success");
+
+    	if(authenticNum==num){
+			resultMap.put("message", "success");
+		}else{
+			resultMap.put("message", "fail");
+		}
     	status = HttpStatus.OK;
 		return new ResponseEntity<Map<String, Object>>(resultMap, status);
 	}
