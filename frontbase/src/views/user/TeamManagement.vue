1c447fcd34f5dda6dfb7fcd002939ca606ccff93
bumhee <qqq960909@likelion.org>
2021-08-09 17:42:06 +0900

    [S05P13C105-235][FE] fix : 팀 관리 페이지

--- frontbase/src/views/user/TeamManagement.vue
+++ frontbase/src/views/user/TeamManagement.vue
@@ -1,48 +1,65 @@
 <template>
   <div>
     <h1>팀원 조회 및 팀장 권환 부여</h1>
-    <template>
-      <v-card>
-        <v-card-title>
-          <v-text-field
-            v-model="search"
-            append-icon="search"
-            label="Search"
-            single-line
-            hide-details
-          ></v-text-field>
-        </v-card-title>
-        <v-data-table
-          v-model="selected"
-          :headers="headers"
-          :items="items"
-          item-key="name"
-          :search="search"
-          class="elevation-1"
+    <div>
+      <template>
+        <v-card>
+          <v-card-title>
+            <v-text-field
+              v-model="search"
+              append-icon="search"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-card-title>
+          <v-data-table
+            v-model="selected"
+            :headers="headers"
+            :items="items"
+            item-key="name"
+            select-all
+            hide-actions
+            :pagination.sync="pagination"
+            :search="search"
+            class="elevation-1"
+          >
+            <template v-slot:items="props">
+              <td>
+                <v-checkbox
+                  v-model="props.selected"
+                  primary
+                  hide-details
+                ></v-checkbox>
+              </td>
+              <td>{{ props.item.name }}</td>
+              <td class="text-xs-right">{{ props.item.point }}</td>
+              <td class="text-xs-right">{{ props.item.email }}</td>
+              <td class="text-xs-right">{{ props.item.phone }}</td>
+              <td class="text-xs-right">{{ props.item.mbti }}</td>
+              <td class="text-xs-right">{{ props.item.date }}</td>
+            </template>
+            <template v-slot:no-results>
+              <v-alert :value="true" color="error" icon="warning">
+                Your search for "{{ search }}" found no results.
+              </v-alert>
+            </template>
+          </v-data-table>
+          <div class="text-xs-center pt-2">
+            <v-pagination v-model="pagination.page" :length="pages"></v-pagination>
+          </div>
+        </v-card>
+      </template>
+      <div class="text-center pt-2">
+        <v-btn
+          color="primary"
+          class="mr-2"
+          @click="chageleader"
         >
-          <template v-slot:items="props">
-            <td>
-              <v-checkbox
-                v-model="props.selected"
-                primary
-                hide-details
-              ></v-checkbox>
-            </td>
-            <td>{{ props.item.name }}</td>
-            <td class="text-xs-right">{{ props.item.point }}</td>
-            <td class="text-xs-right">{{ props.item.email }}</td>
-            <td class="text-xs-right">{{ props.item.phone }}</td>
-            <td class="text-xs-right">{{ props.item.mbti }}</td>
-            <td class="text-xs-right">{{ props.item.date }}</td>
-          </template>
-          <template v-slot:no-results>
-            <v-alert :value="true" color="error" icon="warning">
-              Your search for "{{ search }}" found no results.
-            </v-alert>
-          </template>
-        </v-data-table>
-      </v-card>
-    </template>
+          팀장 부여
+        </v-btn>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -52,6 +69,7 @@ import { mapGetters } from "vuex";
 export default {
   name: "TeamManagement",
   data: () => ({
+    pagination: {},
     selected: [],
     search: '',
     headers: [
@@ -100,18 +118,49 @@ export default {
         mbti: 'iiii',
         date: '2021-08-09',
       },
+      {
+        name: '이범희',
+        point: 80,
+        email: 'qqq960909@naver.com',
+        phone: '010-4533-0093',
+        mbti: 'iiii',
+        date: '2021-08-09',
+      },
+      {
+        name: '이범희',
+        point: 200,
+        email: 'qqq960909@naver.com',
+        phone: '010-4533-0093',
+        mbti: 'iiii',
+        date: '2021-08-09',
+      },
+      {
+        name: '이범희',
+        point: 200,
+        email: 'qqq960909@naver.com',
+        phone: '010-4533-0093',
+        mbti: 'iiii',
+        date: '2021-08-09',
+      },
     ],
   }),
   computed: {
+    ...mapGetters(["managingTeamMembers", "managingTeam"]),
+    pages () {
+        if (this.pagination.rowsPerPage == null ||
+          this.pagination.totalItems == null
+        ) return 0
+
+        return Math.ceil(this.pagination.totalItems / this.pagination.rowsPerPage)
+      },
   },
   created() {
-
+    this.$store.dispatch("getTeamMembers", this.managingTeam.teamId);
+    console.log(this.managingTeamMembers);
   },
   methods: {
-    getColor (point) {
-      if (point > 200) return 'red'
-      else if (point > 100) return 'orange'
-      else return 'green'
+    chageleader() {
+
     },
   },
 };
