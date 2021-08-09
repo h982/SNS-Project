bf8dfa56b5c0f95701ea806d10ac0c296ec69c81
진해진 <jhj030812@naver.com>
2021-08-09 14:11:54 +0900

    [S05P13C105-113][FE] feat : 수정, 삭제 버튼 추가

--- frontbase/src/views/Feed/FeedItem.vue
+++ frontbase/src/views/Feed/FeedItem.vue
@@ -5,6 +5,13 @@
       <div class="feed_date">{{ feed.writeDate }}</div>
     </div>
     <div class="feed-card">
+      <div class="feed-btns">
+        <div class="feed-show-btn" @click="showBtns"></div>
+        <div class="feed-change">
+          <div>수정</div>
+          <div>삭제</div>
+        </div>
+      </div>
       <div class="feed-wrap">
         <div
           class="img"
@@ -30,6 +37,7 @@
 import { mapGetters } from "vuex";
 import defaultImage from "../../assets/images/img-placeholder.png";
 import defaultProfile from "../../assets/images/profile_default.png";
+
 export default {
   props: ["feed"],
   data: () => {
@@ -45,6 +53,14 @@ export default {
       this.$store.dispatch("GET_COMMENTS", this.feed.feedId);
       this.$store.dispatch("SET_FEEDID", this.feed.feedId);
       this.$router.replace("/comment");
+    },
+    showBtns() {
+      var showitem = document.querySelector(".feed-change");
+      if (showitem.style.display == "none") {
+        showitem.style.display = "block";
+      } else {
+        showitem.style.display = "none";
+      }
     }
   }
 };
