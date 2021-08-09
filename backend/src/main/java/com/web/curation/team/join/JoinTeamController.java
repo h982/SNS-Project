1873cf8ecbedf04d12be9b6b0a8feb28adc8f982
양동현 <y7248527@naver.com>
2021-08-09 15:51:20 +0900

    [S05P13C105-236][BE] feat : 팀 멤버 리스트

--- backend/src/main/java/com/web/curation/team/join/JoinTeamController.java
+++ backend/src/main/java/com/web/curation/team/join/JoinTeamController.java
@@ -5,6 +5,9 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,5 +30,15 @@ public class JoinTeamController {
     	}
     		
     	return new ResponseEntity<>(HttpStatus.CONFLICT);
+    }
+    
+    @GetMapping("/jointeam/member/{teamId}")
+    @ApiOperation(value = "팀 멤버 리스트 호출")
+    public Object getMyTeamMember(@PathVariable int teamId) {
+    	Map<String, Object> resultMap = new HashMap<>();
+		
+		resultMap.put("data", joinTeamService.findMemberByTeam(teamId));
+		resultMap.put("message", "success");
+		return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.OK);
     }
 }
