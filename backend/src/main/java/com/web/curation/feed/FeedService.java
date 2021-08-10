6074dbd3ca425a7f12e21549b5880da149c71480
hohwang <ghghkd52@naver.com>
2021-08-10 10:04:02 +0900

    [S05P13C105-248][BE] refactor: Feed Service code Refactor

--- backend/src/main/java/com/web/curation/feed/FeedService.java
+++ backend/src/main/java/com/web/curation/feed/FeedService.java
@@ -76,10 +76,10 @@ public class FeedService {
                 .orElseThrow(() -> new CustomException(MEMBER_NOT_FOUND));
         List<JoinTeam> joinTeamList = joinTeamDao.findJoinTeamByMember(chkMember);
         List<Team> teamForSearch = new ArrayList<>();
-        for (JoinTeam joinTeam : joinTeamList) {
-            teamForSearch.add(joinTeam.getTeam());
-        }
-
+//        for (JoinTeam joinTeam : joinTeamList) {
+//            teamForSearch.add(joinTeam.getTeam());
+//        }
+        joinTeamList.forEach(joinTeam -> teamForSearch.add(joinTeam.getTeam()));
         List<Feed> feeds = feedDao.findAllJoinFetch(teamForSearch, pageRequest);
         if (feeds == null) {
             return Collections.emptyList();
