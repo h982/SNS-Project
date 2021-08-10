7d513489ccb7fcbb1e6bed34906a9db9da214436
진해진 <jhj030812@naver.com>
2021-08-10 16:13:37 +0900

    [S05P13C105-113[FE] feat : 피드 삭제

--- frontbase/src/views/Feed/FeedItem.vue
+++ frontbase/src/views/Feed/FeedItem.vue
@@ -6,9 +6,9 @@
     </div>
     <div class="feed-card">
       <div class="feed-btns">
-        <div class="feed-show-btn" @click="showBtns"></div>
+        <div class="feed-show-btn"></div>
         <div class="feed-change">
-          <div @click="modifyFeed">수정</div>
+          <div @click="modifyFeed(feed.feedId)">수정</div>
           <div @click="deleteFeed">삭제</div>
         </div>
       </div>
@@ -35,13 +35,21 @@
 
 <script>
 import { mapGetters } from "vuex";
+import { createInstance } from "@/api/teamindex.js";
 import defaultImage from "../../assets/images/img-placeholder.png";
 import defaultProfile from "../../assets/images/profile_default.png";
 
 export default {
   props: ["feed"],
   data: () => {
-    return { defaultImage, defaultProfile };
+    return {
+      defaultImage,
+      defaultProfile,
+      feedget: {
+        memberId: "",
+        page: ""
+      }
+    };
   },
   computed: {
     ...mapGetters(["memberInfo", "myTeamList"])
@@ -57,17 +65,23 @@ export default {
       this.$store.dispatch("SET_FEEDID", this.feed.feedId);
       this.$router.replace("/comment");
     },
-    showBtns() {
-      var showitem = document.querySelector(".feed-change");
-      if (showitem.style.display == "none") {
-        showitem.style.display = "block";
-      } else {
-        showitem.style.display = "none";
-      }
-    },
-    modifyFeed() {
+    // showBtns() {
+    //   var showitem = document.querySelector(".feed-change");
+    //   showitem.style.display = "block";
+    //   // if (showitem.style.display == "none") {
+    //   //   showitem.style.display = "block";
+    //   // } else {
+    //   //   showitem.style.display = "none";
+    //   // }
+    // },
+    modifyFeed(data) {
+      console.log(data);
       if (this.feed.writer.replaceAll('"', "") === this.memberInfo.name) {
         this.$store.dispatch("SET_FEEDID", this.feed.feedId);
+        this.$store.dispatch(
+          "GET_TEAMCHALLENGEING_INFO",
+          this.memberInfo.memberId
+        );
         const body = {
           memberId: this.memberInfo.memberId,
           teamId: this.myTeamList[0].text.teamId,
@@ -85,6 +99,39 @@ export default {
     },
     deleteFeed() {
       if (this.feed.writer.replaceAll('"', "") === this.memberInfo.name) {
+        console.log(this.feed.feedId);
+
+        // const formData = new FormData();
+        // formData.append("feedId", this.feed.feedId);
+        // formData.append("teamchallengeId", 0);
+        // formData.append("memberId", this.feed.member.memberId);
+        // formData.append("teamId", this.feed.team.teamId);
+        // formData.append("teamName", this.feed.teamName);
+        // formData.append("contents", this.feed.contents);
+        // formData.append("writer", this.feed.writer);
+        // formData.append("image", this.feed.photos[0]);
+
+        const instance = createInstance();
+        instance
+          .delete("/feed/" + this.feed.feedId)
+          .then(response => {
+            if (response.data.data === "success") {
+              this.$store.dispatch(
+                "GET_TEAMCHALLENGEING_INFO",
+                this.memberInfo.memberId
+              );
+              alert("피드 삭제 완료");
+              this.feedget.memberId = this.memberInfo.memberId;
+              this.feedget.page = 0;
+              this.$store.dispatch("getFeeds", this.feedget);
+              this.$router.push("/feed");
+            } else {
+              alert("피드 삭제 실패");
+            }
+          })
+          .catch(() => {
+            alert("에러");
+          });
       } else {
         alert("본인만 삭제할 수 있습니다");
       }
