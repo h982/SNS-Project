41beaffafdaad8fd537bdb7f5dc0918b0ec3cd8f
KingBlackCow <sgs1159@naver.com>
2021-08-11 15:54:18 +0900

    [S05P13C105-184][BE] feat : naver 로그인 백엔드 구현

--- backend/src/main/java/com/web/curation/member/MemberController.java
+++ backend/src/main/java/com/web/curation/member/MemberController.java
@@ -1,5 +1,6 @@
 package com.web.curation.member;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -9,6 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -18,6 +22,7 @@ import com.web.curation.member.challenge.ChallengeService;
 
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
@@ -40,35 +45,52 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
 @CrossOrigin(origins = {"*"}, maxAge = 6000)
 @RestController
 @RequestMapping("/member")
 public class MemberController {
-	// test1
+    // test1
     @Autowired
     MemberService memberService;
-    
+
     @Autowired
     ChallengeService challengeService;
 
     @Autowired
     private JwtServiceImpl jwtService;
 
+    private NaverLoginBO naverLoginBO;
+    private String apiResult = null;
+
+    @Autowired
+    private void setNaverLoginBO(NaverLoginBO naverLoginBO) {
+        this.naverLoginBO = naverLoginBO;
+    }
+
+    HttpSession rootSession;
+
+
     EntityManager em;
 
     @PostMapping("/signup")
     @ApiOperation(value = "회원가입")
     @ApiResponses(value = {@ApiResponse(code = 201, message = "회원이 생성됨", response = BasicResponse.class),
-    						@ApiResponse(code = 409, message = "중복된 값이 있음", response = BasicResponse.class)})
+            @ApiResponse(code = 409, message = "중복된 값이 있음", response = BasicResponse.class)})
     public ResponseEntity<Map<String, Object>> signup(@RequestBody MemberDto memberDto) {
         Map<String, Object> resultMap = new HashMap<>();
         HttpStatus status = HttpStatus.ACCEPTED;
-        
+
         memberDto.setAuthenticated(false);
         if (!memberService.hasSameEmail(memberDto.getEmail())) {
             MemberDto responseMemberDto = memberService.registMember(memberDto);
             challengeService.createChallenge(responseMemberDto.getMemberId());
-            
+
             resultMap.put("message", "success");
             status = HttpStatus.CREATED;
             System.out.println(resultMap.get("message"));
@@ -87,10 +109,10 @@ public class MemberController {
         Map<String, Object> resultMap = new HashMap<>();
         HttpStatus status = null;
         try {
-            Optional<MemberDto> loginUser = memberService.getUser(memberDto.getEmail(),memberDto.getPassword());
+            Optional<MemberDto> loginUser = memberService.getUser(memberDto.getEmail(), memberDto.getPassword());
             if (loginUser.isPresent()) {
-            	challengeService.attend(loginUser.get().getMemberId());
-            	
+                challengeService.attend(loginUser.get().getMemberId());
+
                 String token = jwtService.create("memberEmail", loginUser.get().getEmail(), "access-token");// key, data, subject
                 resultMap.put("access-token", token);
                 resultMap.put("message", "success");
@@ -132,35 +154,35 @@ public class MemberController {
         }
         return new ResponseEntity<Map<String, Object>>(resultMap, status);
     }
-    
+
     @ApiOperation(value = "회원정보 수정")
     @PutMapping
-    public ResponseEntity<Map<String, Object>> updateInfo(@RequestBody MemberDto memberDto){
+    public ResponseEntity<Map<String, Object>> updateInfo(@RequestBody MemberDto memberDto) {
         Map<String, Object> resultMap = new HashMap<>();
         Optional<MemberDto> responseMemberDto = memberService.updateMember(memberDto);
-        
-        if(responseMemberDto.isPresent()) {
-        	Map<String, Object> dataMap = new HashMap<>();
-        	dataMap.put("memberInfo", responseMemberDto);
-        	resultMap.put("data", dataMap);
+
+        if (responseMemberDto.isPresent()) {
+            Map<String, Object> dataMap = new HashMap<>();
+            dataMap.put("memberInfo", responseMemberDto);
+            resultMap.put("data", dataMap);
             resultMap.put("message", "success");
-            
+
             return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.OK);
         }
 
         resultMap.put("message", "해당하는 email이 없습니다.");
         return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.NOT_FOUND);
     }
-    
+
     @ApiOperation(value = "카카오 로그인")
     @GetMapping("/kakao")
-    public ResponseEntity<Map<String, Object>> kakaoLogin(@RequestParam String code){
+    public ResponseEntity<Map<String, Object>> kakaoLogin(@RequestParam String code) {
         Map<String, Object> resultMap = new HashMap<>();
-        
+
         String token = getKakaoToken(code);
         Map<String, String> userInfo = getKaKaoUserInfo(token);
         Optional<MemberDto> dto = memberService.getMemberByEmail(userInfo.get("email"));
-        
+
         resultMap.put("message", "카카오 유저 정보");
         resultMap.put("data", userInfo);
         resultMap.put("member", dto);
@@ -168,37 +190,37 @@ public class MemberController {
         return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.OK);
     }
 
-	private String getKakaoToken(String code) {
-		String access_Token = "";
-		String refresh_Token = "";
-		HttpURLConnection conn = null;
-		
-		try {
-			URL url = new URL("https://kauth.kakao.com/oauth/token");
-			conn = (HttpURLConnection)url.openConnection();
-			
-			conn.setRequestMethod("POST");
-			conn.setDoOutput(true);
-			conn.setRequestProperty("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-			
-			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
-			StringBuilder sb = new StringBuilder();
-			sb.append("grant_type=authorization_code");
-			sb.append("&client_id=4ae2f007d84e033a263932b5176355f1");
-			sb.append("&redirect_uri=http://localhost:8081/kakaosignup");
-			sb.append("&code=" + code);
-			bw.write(sb.toString());
-			bw.flush();
-			
-			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String line = "";
-			String result = "";
-			
-			while((line = br.readLine()) != null) {
-				result += line;
-			}
-			
-			JsonParser parser = new JsonParser();
+    private String getKakaoToken(String code) {
+        String access_Token = "";
+        String refresh_Token = "";
+        HttpURLConnection conn = null;
+
+        try {
+            URL url = new URL("https://kauth.kakao.com/oauth/token");
+            conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+            StringBuilder sb = new StringBuilder();
+            sb.append("grant_type=authorization_code");
+            sb.append("&client_id=4ae2f007d84e033a263932b5176355f1");
+            sb.append("&redirect_uri=http://localhost:8081/kakaosignup");
+            sb.append("&code=" + code);
+            bw.write(sb.toString());
+            bw.flush();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = "";
+            String result = "";
+
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+
+            JsonParser parser = new JsonParser();
             JsonElement element = parser.parse(result);
 
             access_Token = element.getAsJsonObject().get("access_token").getAsString();
@@ -206,14 +228,14 @@ public class MemberController {
 
             br.close();
             bw.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		return access_Token;
-	}
-	
-	public Map<String, String> getKaKaoUserInfo(String access_Token) {
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return access_Token;
+    }
+
+    public Map<String, String> getKaKaoUserInfo(String access_Token) {
 
         Map<String, String> userInfo = new HashMap<>();
         String reqURL = "https://kapi.kakao.com/v2/user/me";
@@ -238,7 +260,7 @@ public class MemberController {
             JsonElement element = parser.parse(result);
 
             JsonObject kakao_account = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
-            
+
             String id = element.getAsJsonObject().get("id").getAsString();
             String email = null;
             if (kakao_account.getAsJsonObject().get("email") != null) {
@@ -252,5 +274,52 @@ public class MemberController {
         }
 
         return userInfo;
+    }
+
+    @RequestMapping(value = "/naverlogin", method = {RequestMethod.GET, RequestMethod.POST})
+    public String login(Model model, HttpSession session) {
+        rootSession = session;
+        String naverAuthUrl = naverLoginBO.getAuthorizationUrl(session);
+        model.addAttribute("url", naverAuthUrl);
+        return naverAuthUrl;
+    }
+
+    @RequestMapping(value = "/navercallback", method = {RequestMethod.GET, RequestMethod.POST})
+    public ResponseEntity<Map<String, Object>> callback(Model model, @RequestParam String code, @RequestParam String state, HttpSession session) throws IOException, ParseException {
+        OAuth2AccessToken oauthToken;
+        oauthToken = naverLoginBO.getAccessToken(rootSession, code, state);
+        apiResult = naverLoginBO.getUserProfile(oauthToken);
+
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(apiResult);
+        JSONObject jsonObj = (JSONObject) obj;
+        JSONObject response_obj = (JSONObject) jsonObj.get("response");
+        String email = (String) response_obj.get("email");
+        String phone = (String) response_obj.get("mobile");
+        String name = (String) response_obj.get("name");
+        phone = phone.replaceAll("-", "");
+
+        session.setAttribute("sessionId", email);
+        model.addAttribute("result", apiResult);
+
+        Map<String, Object> resultMap = new HashMap<>();
+        HttpStatus status = HttpStatus.ACCEPTED;
+
+        Optional<MemberDto> member = memberService.getMemberByEmail(email);
+        if (member.isPresent()) {
+            String token = jwtService.create("memberEmail", email, "access-token");
+            resultMap.put("access-token", token);
+            resultMap.put("message", "nocreate");
+            status = HttpStatus.CREATED;
+        } else {
+            resultMap.put("message", "needcreate");
+            resultMap.put("email", email);
+            resultMap.put("name", name);
+            resultMap.put("phone", phone);
+            status = HttpStatus.CREATED;
+        }
+
+        System.out.println(resultMap.get("message"));
+        return new ResponseEntity<Map<String, Object>>(resultMap, status);
     }
 }
