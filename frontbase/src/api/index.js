502f87e3493b13e8ceba31f1cb860eb6adf1e061
KingBlackCow <sgs1159@naver.com>
2021-08-13 16:06:40 +0900

    [S05P13C105-254][FE] fix : refactoring

--- frontbase/src/api/index.js
+++ frontbase/src/api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 function createInstance() {
   const instance = axios.create({
     //baseURL: "http://localhost:8080",
-    baseURL: "http://i5c105.p.ssafy.io",
+    baseURL: "http://i5c105.p.ssafy.io/api",
 
     headers: {
       "Content-Type": "application/json"
--- frontbase/src/api/teamindex.js
+++ frontbase/src/api/teamindex.js
@@ -5,7 +5,7 @@ import store from "@/store/index";
 function createInstance() {
   const instance = axios.create({
     //baseURL: "http://localhost:8080",
-    baseURL: "http://i5c105.p.ssafy.io",
+    baseURL: "http://i5c105.p.ssafy.io/api",
     headers: {
       "Content-Type": "application/json",
       "access-token":store.state.token,
