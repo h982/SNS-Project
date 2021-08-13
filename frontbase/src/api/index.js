5dab0df3037c637bb3e1c3a58503c9652357d5e7
KingBlackCow <sgs1159@naver.com>
2021-08-13 15:51:52 +0900

    [S05P13C105-254][FE] fix : refactoring

--- frontbase/src/api/index.js
+++ frontbase/src/api/index.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 function createInstance() {
   const instance = axios.create({
-    baseURL: "http://localhost:8080",
+    //baseURL: "http://localhost:8080",
+    baseURL: "http://i5c105.p.ssafy.io",
+
     headers: {
       "Content-Type": "application/json"
     },
