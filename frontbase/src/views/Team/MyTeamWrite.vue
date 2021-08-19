9f64c9ac7927a0cb5f3e099ebedeae11f27d8e93
ericharry <ericharry@naver.com>
2021-08-19 14:20:20 +0900

    [S05P13C105-374][FE] bug : MyTeamWrite 문구 수정

--- frontbase/src/views/Team/MyTeamWrite.vue
+++ frontbase/src/views/Team/MyTeamWrite.vue
@@ -1,39 +1,40 @@
 <template>
   <v-container grid-list-xl>
-      <v-layout column justify-center align-center data-aos="fade-right">
-        <v-toolbar-title class="headline">
-            <span><b>현재</b></span>
-            <span class="green--text"><b>&nbsp;가입한팀</b></span>
-        </v-toolbar-title>
-      </v-layout>
-      <v-layout justify-center align-center data-aos="fade-up">
-        <v-toolbar-title class="headline">
-          <b>팀을 먼저</b><span class="green--text"><b>&nbsp;선택해주세요</b></span>
-        </v-toolbar-title>
-      <br><br><br><br>
-      </v-layout>
+    <v-layout column justify-center align-center data-aos="fade-right">
+      <v-toolbar-title class="headline">
+        <span><b>피드를 </b></span>
+        <span><b>&nbsp;작성할 </b></span>
+        <span class="green--text"><b>팀을&nbsp;선택해주세요</b></span>
+      </v-toolbar-title>
+    </v-layout>
 
-      <v-layout justify-center row wrap class="mt-0 pt-2" data-aos="fade-down" v-if="myTeamList">
-        <PostPreviewWrite
-          v-for="post in myTeamList"
-          :key="post.text.teamId"
-          :name="post.text.name"
-          :introduction="post.text.introduction"
-          :imgPath="post.text.photoDto.filePath"
-          :teamId="post.text.teamId"
-          :selectMyTeam="post.text"
-          :leader="post.text.leader"
-        />
+    <v-layout
+      justify-center
+      row
+      wrap
+      class="mt-0 pt-2"
+      data-aos="fade-down"
+      v-if="myTeamList"
+    >
+      <PostPreviewWrite
+        v-for="post in myTeamList"
+        :key="post.text.teamId"
+        :name="post.text.name"
+        :introduction="post.text.introduction"
+        :imgPath="post.text.photoDto.filePath"
+        :teamId="post.text.teamId"
+        :selectMyTeam="post.text"
+        :leader="post.text.leader"
+      />
+    </v-layout>
 
-      </v-layout>
-
-      <v-layout justify-center align-center data-aos="fade-top" v-else>
-        <h1><b>가입된</b><span class="green--text"><b>&nbsp;팀이 없습니다.</b></span></h1>
-      </v-layout>
-
-      
+    <v-layout justify-center align-center data-aos="fade-top" v-else>
+      <h1>
+        <b>가입된</b
+        ><span class="green--text"><b>&nbsp;팀이 없습니다.</b></span>
+      </h1>
+    </v-layout>
   </v-container>
-  
 </template>
 
 <script>
@@ -45,17 +46,15 @@ export default {
     PostPreviewWrite
   },
   computed: {
-    ...mapGetters(["myTeamList","memberInfo"]),
+    ...mapGetters(["myTeamList", "memberInfo"])
   },
   created() {
     this.$store.dispatch("GET_TEAMCHALLENGEING_INFO", this.memberInfo.memberId);
-    this.$store.dispatch("GET_MY_TEAM_INFO",this.memberInfo.memberId);
+    this.$store.dispatch("GET_MY_TEAM_INFO", this.memberInfo.memberId);
     console.log(this.myTeamList);
   },
   data() {
-    return {
-      
-    };
+    return {};
   },
   methods: {
     running() {
@@ -71,9 +70,9 @@ export default {
       this.selected = 3;
     },
     setFilter: function(filter) {
-			this.currentFilter = filter;
-		},
-    confirm(){
+      this.currentFilter = filter;
+    },
+    confirm() {
       alert(this.myTeamList[0].text.imgPath);
       console.log(this.myTeamList);
     },
@@ -84,11 +83,8 @@ export default {
       );
       this.$router.push("/writefeed");
     }
-    
-  },
+  }
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
